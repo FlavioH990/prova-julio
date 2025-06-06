3366dab012704b07726f6e6a1656e6c5ed46bc59
FlavioH990 <fhs5895santos@outlook.com>
2025-06-05 22:35:59 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
+import os
+print(os.getcwd())
 
-
-df = pd.read_csv("MS_Financial Sample.csv")
+df = pd.read_csv("MS_Financial Sample.csv", sep=";")
 
 st.title("Gráfico simples de Vendas por Categoria")
+
 
 
 vendas = df.groupby("Product Category")["Revenue"].sum()
